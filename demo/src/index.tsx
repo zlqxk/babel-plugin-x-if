bb3308f173b3f7f760a30f04314eca67ffc3fe59
zhanglu <809740443@qq.com>
2021-03-05 01:11:10 +0800

    fix: 支持自闭和标签

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+interface TestProps {
+  className?: string;
+  title?: string;
+}
+
+const Test: React.FC<TestProps> = props => {
+  return <div>{props.title}</div>;
+};
+
+const App: React.FC = () => {
+  const [visible, setVisible] = useState<boolean>(true);
+  const [visible2, setVisible2] = useState<boolean>(true);
+  const [visibleTest, setVisibleTest] = useState<boolean>(true);
+
+  return (
+    <div>
+      <div x-if={visible}>
+        1111
+        <div x-if={visible2}>2222</div>
+      </div>
+      <Test x-if={visibleTest} title="test" />
+      <img x-if={visible} src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2969235134,2098148338&fm=11&gp=0.jpg" alt=""/>
+      <button
+        onClick={() => {
+          setVisible(c => !c);
+        }}
+      >
+        修改1
+      </button>
+      <button
+        onClick={() => {
+          setVisible2(c => !c);
+        }}
+      >
+        修改2
+      </button>
+      <button
+        onClick={() => {
+          setVisibleTest(c => !c);
+        }}
+      >
+        修改Test
+      </button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- index.js
+++ index.js
@@ -3,21 +3,21 @@ module.exports = function ({ types: t }) {
     visitor: {
       JSXElement(path) {
         const { node } = path;
-        const xIfAttr = node.openingElement.attributes.find((item) => {
+        const xIfAttr = node.openingElement.attributes.find(item => {
           return item.type === "JSXAttribute" && item.name.name === "x-if";
         });
         if (!xIfAttr) return;
         const openElement = t.jSXOpeningElement(
           node.openingElement.name,
-          node.openingElement.attributes.filter((item) => {
+          node.openingElement.attributes.filter(item => {
             return item.name.name !== "x-if";
-          })
+          }),
+          node.openingElement.selfClosing
         );
         const Element = t.JSXElement(
           openElement,
           node.closingElement,
-          node.children,
-          false
+          node.children
         );
         const expression = t.conditionalExpression(
           xIfAttr.value.expression,
